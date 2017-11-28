9cf6d56e458316b9c111c9c62491102b072c2342
vikas <vikasnikam05@gmail.com>
2017-11-28 10:21:17 +0530

    Initial commit

---
+++ app/src/main/java/com/vikas/codetest/MainActivity.java
@@ -0,0 +1,23 @@
+package com.vikas.codetest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+   private   static  String TAG="MainActivity";
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+    }
+}
